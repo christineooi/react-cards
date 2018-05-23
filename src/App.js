59b0ea90b1878cd99f310f4e44fef0659280feba
Christine Ooi <christine.ooi@gmail.com>
2018-05-23 16:29:24 -0400

    Fixed conditional rendering with hint

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const Card = (props) => (
         <div className="card__description cardGroup__cardDescription">
             <div className={`icon fa ${props.icon} card__descriptionIcon`} />
             <div className="card__descriptionText">
-                {props.description} { props.hint && <p> {props.hint} </p>}
+                {props.description} {props.hint && <React.Fragment><br/>({props.hint})</React.Fragment>}
             </div>
         </div>
         <div className="card__price">{props.price}</div>
